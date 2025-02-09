49e99078e0ea8fa00b3108370e96d0c402ea08bf
maryregan <mrr5943@psu.edu>
2025-02-09 01:39:18 -0500

    Your commit message

--- src/my-card.js
+++ src/my-card.js
@@ -15,23 +15,70 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.image = "https://t3.ftcdn.net/jpg/07/08/87/22/360_F_708872253_fnARsh5Ds0rOQrsFUeGRQML6Ethg8kIW.jpg";
+    this.content = "This is a birthday cat! Happy birthday!!";
+    this.link = "https://hax.psu.edu";
+    this.buttonText = "Click me!";
+    this.color = "powder blue";
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        max-width: 625px;
+        margin: 16px;
+        border: 1px solid #000;
+        border-radius: 8px;
+        padding: 16px;
+        background-color: var(--card-color, white);
+      }
+      .control-wrapper {
+        display: flex;
+        margin-bottom: 16px;
+      }
+      .birthday-card {
+        border: 5px solid black; 
+        background-color: powderblue; 
+        max-width: 400px;
+        }
+        .btn {
+        background-color: blue;
+        color: white;
+        font-size: 20px;
+        border-radius: 10%;
+        padding: 16px;
+        margin: 4px 50px 10px 70px;
+        width: 200px;
       }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+      <div id="card" style="background-color: ${this.color};">
+      <div class="birthday-card">
+        <img class="card-image" alt="Birthday" src="${this.image}" style="width:350px; height:200px;">
+        <div class="text">  
+        <h1 class="card-title">${this.title}</h1>
+        <p>${this.content}</p>
+        <a href="${this.link}" target="_blank">
+          <button class="btn">${this.buttonText}</button>
+        </a>
+        </div>
+      </div>
+      </div>
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      content: { type: String },
+      link: { type: String },
+      buttonText: { type: String },
+      color: { type: String },
     };
   }
 }
